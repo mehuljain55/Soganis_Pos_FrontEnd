1f3ca4792bcb83b68a468238a758bf5f56749cac
Mehul Jain <97732513+mehuljain55@users.noreply.github.com>
2024-12-28 20:35:01 +0530

    Feature Update

--- src/UserComponents/Inventory/AddInventoryItem.js
+++ src/UserComponents/Inventory/AddInventoryItem.js
@@ -24,6 +24,7 @@ function AddInventoryItem() {
   const handleClearFile = () => {
     setFile(null);
     setItems([]);
+    setStatus("");
   };
 
   const handleQuantityChange = (index, newQuantity) => {
