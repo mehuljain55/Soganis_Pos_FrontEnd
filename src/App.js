ba310827398e60bb7190180bd21845afd0efa073
Mehul Jain <97732513+mehuljain55@users.noreply.github.com>
2025-05-17 02:32:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -18,8 +18,6 @@ import Invoice from './UserComponents/Invoice/Invoice';
 import SchoolSalesReport from './UserComponents/SalesReport/SchoolSalesReport'; 
 import SalesReportGraph from './UserComponents/SalesReport/SalesReportGraph'; 
 import ExchangeBill from './UserComponents/Billing/ExchangeBill.js';
-import ServerUpdate from './UserComponents/Dashboard/ServerUpdate.js';
-
 import './App.css'; 
 
 
@@ -98,7 +96,6 @@ function App() {
           ) : (
             <>
               <Route path="/" element={<MainComponent userData={userData} />} />
-              <Route path="/server/update" element={<ServerUpdate />} />
               <Route path="/barcode" element={<BarcodePrintPage />} />
             </>
           )}
